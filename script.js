65b21e14293d6cf29cb84ff157c9040340f1a532
Darshna76 <117654044+Darshna76@users.noreply.github.com>
2023-04-13 22:37:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,10 +2,11 @@
 const student={
 	name:"Darshna"
 };
-Object.prototype.getKeys=function(){
-	return Object.Keys(this);
-}
+Object.prototype.getKeys = function() {
+  // return an array of all keys in the object
+  return Object.keys(this);
+};
 console.log(student.getKeys());
-}
+
 
 
